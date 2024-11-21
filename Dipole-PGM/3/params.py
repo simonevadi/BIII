1fe70f3b563038f8b74dbea4ed8cb0c7b06a119f
Simone Vadilonga <simone.vadilonga@helmholtz-berlin.de>
2024-11-21 18:00:01 +0100

    update

--- Dipole-PGM/3/params.py
+++ Dipole-PGM/3/params.py
@@ -1,8 +1,9 @@
 import numpy as np
 import os
 
+rounds=10
+cpu = 30
 # BESSYIII params
-
 # possible values for coupling
 # 2, 10, 50, 75, 100
 coupling = 2 
@@ -17,12 +18,12 @@ hb_1200_SlitSize    = np.array([0.1])
 hb_1200_grating     = np.array([1200])
 hb_1200_blaze       = np.array([0.9])
 hb_1200_cff         = np.array([2.25])
-hb_1200_nrays_flux  = 2e5
-hb_1200_nrays_rp    = 2e5 
-hb_1200_rounds_flux = 50
-hb_1200_rounds_rp   = 50
-hb_1200_ncpu_flux   = 30
-hb_1200_ncpu_rp     = 30
+hb_1200_nrays_flux  = 1e5
+hb_1200_nrays_rp    = 1e5 
+hb_1200_rounds_flux = rounds
+hb_1200_rounds_rp   = rounds
+hb_1200_ncpu_flux   = cpu
+hb_1200_ncpu_rp     = cpu
 
 hb_1200_sim_name_flux = '1200_FLUX'
 hb_1200_sim_name_rp   = '1200_RP'
@@ -33,17 +34,17 @@ hb_1200_file_path   = os.path.join('rml/'+hb_1200_rml_file_name+'.rml')
 
 #   PARAMS FOR 400l/mm GRATING SIMULATIONS
 lb_400_order       = 1
-lb_400_energy_flux = np.arange(30, 2551,10)
-lb_400_energy_rp   = np.arange(30, 2551,10)
+lb_400_energy_flux = np.arange(30, 2001,10)
+lb_400_energy_rp   = np.arange(30, 2001,10)
 lb_400_SlitSize    = np.array([0.1])
 lb_400_grating     = np.array([400])
 lb_400_cff         = np.array([2.25])
-lb_400_nrays_flux  = 2e5
-lb_400_nrays_rp    = 2e5 
-lb_400_rounds_flux = 50
-lb_400_rounds_rp   = 50
-lb_400_ncpu_flux   = 30
-lb_400_ncpu_rp     = 30
+lb_400_nrays_flux  = 1e5
+lb_400_nrays_rp    = 1e5 
+lb_400_rounds_flux = rounds
+lb_400_rounds_rp   = rounds
+lb_400_ncpu_flux   = cpu
+lb_400_ncpu_rp     = cpu
 
 lb_400_sim_name_flux = '400_FLUX'
 lb_400_sim_name_rp   = '400_RP'
@@ -61,12 +62,12 @@ ml_energy_flux  = np.arange(500, 5001,500)
 ml_energy_rp    = np.arange(500, 5001,500)
 ml_SlitSize     = np.array([0.1])
 ml_grating      = np.array([2400])
-ml_nrays_flux   = 2e5
-ml_nrays_rp     = 2e5 
-ml_rounds_flux  = 50
-ml_rounds_rp    = 50
-ml_ncpu_flux    = 30
-ml_ncpu_rp      = 30
+ml_nrays_flux   = 1e5
+ml_nrays_rp     = 1e5 
+ml_rounds_flux  = rounds
+ml_rounds_rp    = rounds
+ml_ncpu_flux    = cpu
+ml_ncpu_rp      = cpu
 
 ml_sim_name_rp     = '2400_RP'
 ml_sim_name_flux   = '2400_FLUX'
