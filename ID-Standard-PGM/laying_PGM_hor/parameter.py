e3d7c0b282783df7febf4e70404c905eac8f9ffd
Sebastian Malerz <sebastian.malerz@helmholtz-berlin.de>
2025-05-15 21:20:26 +0200

    add hor PGM simulation with M3 as planar paraboloit

--- ID-Standard-PGM/laying_PGM_hor/parameter.py
+++ ID-Standard-PGM/laying_PGM_hor/parameter.py
@@ -3,8 +3,10 @@ import numpy as np
 # Load in rml file 
 
 # BESSY III:
-rml_file_name_bessy3_56m_errors_on          = 'bessy3_56m_PGM_2Perc_coupl_err_on0_75deg_1200l_V3'
-rml_file_name_bessy3_56m_errors_on_hor_PGM  = 'bessy3_56m_PGM_2Perc_coupl_err_on0_75deg_1200l_hor_PGM'
+# rml_file_name_bessy3_56m_errors_on                = 'bessy3_56m_PGM_2Perc_coupl_err_on0_75deg_1200l_V3'
+# rml_file_name_bessy3_56m_errors_on_hor_PGM        = 'bessy3_56m_PGM_2Perc_coupl_err_on0_75deg_1200l_hor_PGM'
+rml_file_name_bessy3_56m_errors_on_hor_PGM_M3Para   = 'bessy3_56m_PGM_2Perc_coupl_err_on0_75deg_1200l_V3_hor_PGM_M3Paraboloid'
+
 
 # Paramter
 order       = 1
