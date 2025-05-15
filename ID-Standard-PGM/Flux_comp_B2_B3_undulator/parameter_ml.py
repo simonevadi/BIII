37d09ed2ee19330562a85979b38ee856bd3b0d41
Simone Vadilonga <simone.vadilonga@helmholtz-berlin.de>
2025-05-15 09:13:00 +0200

    add simulation files for multilayer coated pgm

---
+++ ID-Standard-PGM/Flux_comp_B2_B3_undulator/parameter_ml.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+
+# BESSY III:
+rml_file_name_bessy3_long_56m_errors_on_ml      = 'bessy3_56m_PGM_2Perc_coupl_err_on0_75deg_2400l_V3'
+
+# Paramter
+order       = 2
+SlitSize    = np.array([.020]) # mm
+grating     = np.array([2400])
+nrays       = 1e5
+rounds      = 10
+ncpu        = 12
+
+grating = pd.read_csv('ML_eff/ELISA_GR2400_2ord_ML-Cr-C_N60_d4.8nm_MLbGR.dat',
+                      sep='\s+')
+mirror = pd.read_csv('ML_eff/ELISA_GR2400_2ord_ML-Cr-C_N60_d4.8nm_MLPM-max.dat',
+                      sep='\s+')
+
+
+cff    = grating['Cff'].to_numpy().flatten()[::10] # take every 10th value
+energy = grating['Energy'].to_numpy().flatten()[::10] # take every 10th value
+
+# Extract efficiency from Andrey's data 
+common_energy = None
+
+if grating['Energy'].equals(mirror['Energy']):
+    # If energy columns match, directly multiply
+    efficiency = pd.DataFrame({
+        'Energy[eV]': grating['Energy'],
+        'Efficiency': grating['Efficiency(GR)'] * mirror['Efficiency(PM)']
+    })
+else:
+    # Interpolate to a common energy range
+    min_energy = max(grating['Energy'].min(), mirror['Energy'].min())
+    max_energy = min(grating['Energy'].max(), mirror['Energy'].max())
+    common_energy = np.linspace(min_energy, max_energy, num=1000)  # Define a common range
+
+    grating_eff = np.interp(common_energy, grating['Energy'], grating['Efficiency(GR)'])
+    mirror_eff = np.interp(common_energy, mirror['Energy'], mirror['Efficiency(PM)'])
+
+    # Create a new DataFrame with interpolated values
+    efficiency = pd.DataFrame({
+        'Energy[eV]': common_energy,
+        'Efficiency': grating_eff * mirror_eff
+    })
+
---
+++ ID-Standard-PGM/Flux_comp_B2_B3_undulator/simulation_2400_ML.py
@@ -0,0 +1,58 @@
+import os
+import pandas as pd
+
+from raypyng import Simulate
+
+# define the values of the parameters to scan 
+from parameter_ml import order, energy
+from parameter_ml import SlitSize
+from parameter_ml import nrays, rounds
+from parameter_ml import ncpu
+from parameter_ml import cff
+from parameter_ml import efficiency
+from parameter_ml import rml_file_name_bessy3_long_56m_errors_on_ml as rml_file_name
+
+sim = Simulate('rml/'+rml_file_name+'.rml', hide=True)
+rml=sim.rml
+beamline = sim.rml.beamline
+
+# define a list of dictionaries with the parameters to scan
+params = [  
+            {beamline.ExitSlit.openingHeight:SlitSize},
+            {beamline.SU.photonEnergy:energy, 
+             beamline.PG.cFactor:cff}, 
+            {beamline.PG.orderDiffraction:order},
+            {beamline.SU.numberRays:nrays}, 
+        ]
+
+#and then plug them into the Simulation class
+sim.params=params
+
+# sim.simulation_folder = '/home/simone/Documents/RAYPYNG/raypyng/test'
+sim.simulation_name = rml_file_name+'_FLUX'
+
+# repeat the simulations as many time as needed
+sim.repeat = rounds
+
+sim.analyze = False # let RAY-UI analyze the results
+sim.raypyng_analysis = True # let RAY-UI analyze the results
+
+## This must be a list of dictionaries
+sim.exports  =  [{beamline.DetectorAtFocus:['RawRaysOutgoing']}]
+
+this_file_dir=os.path.dirname(os.path.realpath(__file__))
+undulator_file_path = os.path.join(this_file_dir,
+                                   'undulator_flux_curves',
+                                   'b2_HiBeta_UE46_2025_smalerz_300mA_flux.txt')
+
+undulator = pd.read_csv(undulator_file_path, delimiter='\t')
+sim.undulator_table=undulator
+
+
+sim.efficiency = efficiency
+
+# create the rml files
+#sim.rml_list()
+
+#uncomment to run the simulations
+sim.run(multiprocessing=ncpu, force=False, remove_round_folders=True, remove_rawrays=True)
