30f05204ed8132d2189411939a4faa7d02ef1ab0
Simone Vadilonga <simone.vadilonga@helmholtz-berlin.de>
2025-07-21 15:07:27 +0200

    add code to calculate multilayer

--- multilayer_monochromator_efficiency/calculate_ml/calc_ml.py
+++ multilayer_monochromator_efficiency/calculate_ml/calc_ml.py
@@ -1,16 +1,47 @@
 import numpy as np
-import matplotlib.pyplot as plt
-
-import xrt.backends.raycing.materials as rm
+from multilayer import MultilayerBragg
 
 
-mSi = rm.Material('Si', rho=2.33)
-mW = rm.Material('W', rho=19.3)
-mL = rm.Multilayer(mSi, 27, mW, 18, 40, mSi)
 
-E = 10000
-theta = np.linspace(0, 2.0, 1001)  # degrees
-rs, rp = mL.get_amplitude(E, np.sin(np.deg2rad(theta)))[0:2]
+# ml = MultilayerBragg(('Cr', 7.19), 23, 
+#                      ('C', 2.3), 27, 
+#                      40, 
+#                      save_recap="ELISA - CrC - 40 layers - test")
 
-plt.plot(theta, abs(rs)**2, 'r', theta, abs(rp)**2, 'b')
-plt.show()
+# theta, rs, rp, bragg_degs = ml.calculate_multilayer(1500,
+#                                                     max_order=1,
+#                                                     angle_range=(0, 20))  # up to 3rd Bragg order
+
+# ml.plot_reflectivity_vs_theta(1500, theta, rs, rp, show_plot=False)
+
+energies = np.arange(500, 8001, 500)  # 30 to 70 keV
+# results_df = ml.calculate_reflectivity_vs_energy(energies, 
+#                                                 order=1, 
+#                                                 window_deg=0.5)
+
+# ml.plot_reflectivity_vs_energy(show_plot=False)
+
+
+# # W/Si multilayer
+# ml_wsi = MultilayerBragg(('W', 19.3), 20, 
+#                          ('Si', 2.33), 20, 
+#                          40,
+#                          save_recap="ELISA - WSi - 40 layers - test")
+
+# results_df_wsi = ml_wsi.calculate_reflectivity_vs_energy(energies, 
+#                                                          order=1, 
+#                                                          window_deg=0.5)
+# ml_wsi.plot_reflectivity_vs_energy(show_plot=False)
+
+# Mo/Si multilayer
+ml_mosi = MultilayerBragg(('Mo', 10.2), 20, 
+                          ('Si', 2.33), 20, 
+                          40,
+                          save_recap="ELISA - MoSi - 40 layers - test")
+
+results_df_mosi = ml_mosi.calculate_reflectivity_vs_energy(energies, 
+                                                           order=1, 
+                                                           window_deg=0.5)
+
+ml_mosi.prepare_raypyng_efficiency_table('MoSi_efficiency')
+ml_mosi.plot_reflectivity_vs_energy(show_plot=False)
