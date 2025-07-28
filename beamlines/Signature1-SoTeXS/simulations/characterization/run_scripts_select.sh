4a04c62bed534335452d5fce24d81ef5a939582f
Simone Vadilonga <simone.vadilonga@helmholtz-berlin.de>
2025-07-28 21:10:37 +0200

    sim

--- beamlines/Signature1-SoTeXS/simulations/characterization/run_scripts_select.sh
+++ beamlines/Signature1-SoTeXS/simulations/characterization/run_scripts_select.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+# Ask for sudo password upfront
+sudo -v
 
 
 
-python simulation_2400_nano_b.py
-python simulation_2400_nano.py
-python simulation_2400.py
+# python simulation_2400_nano_b.py
+# python simulation_2400_nano.py
+# python simulation_2400.py
 
-python simulation_1200.py
-python simulation_1200_nano.py
+# python simulation_1200.py
+# python simulation_1200_nano.py
 
 
+python simulation_2400_nano_b_multi_inc.py
+git add .
+git commit -m 'sim'
+git push
 
+sudo reboot
