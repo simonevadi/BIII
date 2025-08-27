3b43baf4f9e98f4b74d40cd9485070634c985b1e
Simone Vadilonga <simone.vadilonga@helmholtz-berlin.de>
2025-08-27 14:14:05 +0200

    add elisa

---
+++ beamlines/Signature2-Liquid_Interface/simulations/characterization/parameter.py
@@ -0,0 +1,62 @@
+import numpy as np
+import os
+import pandas as pd
+from pathlib import Path
+
+
+this_file_dir   = os.path.dirname(os.path.realpath(__file__))
+ncpu = 10
+nrays=1e5
+rounds = 1
+
+# UNDULATOR
+undulator_file_path = os.path.abspath(
+    os.path.join(Path(__file__).resolve().parents[4], 
+                 'undulators',
+                 'UndulatorFiles_BESSY_III',
+                 'CPMU21_300mA_100pmrad_x_100pmrad.csv')
+)
+
+undulator = pd.read_csv(undulator_file_path)
+
+
+# PARAMS FOR elisa 1200l/mm GRATING SIMULATIONS 3 micron
+e1   = np.arange(500, 2101,1)
+e2   = np.arange(2100, 6001,25)
+energy_1200 = np.concatenate((e1, e2))
+SlitSize_1200 = np.array([.020]) # mm
+cff_1200      = np.array([2.25])
+elisa_1200_sim_name = 'elisa_1200'
+elisa_1200_file_name  = 'elisa_1200'
+elisa_1200_file_path  = os.path.join(Path(__file__).resolve().parents[2],
+                                     'rml',
+                                     elisa_1200_file_name+'.rml')
+
+# PARAMS FOR elisa 2400l/mm GRATING SIMULATIONS 3 micron
+SlitSize_2400 = np.array([.020]) # mm
+elisa_2400_sim_name = 'elisa_2400'
+elisa_2400_file_name  = 'elisa_2400'
+elisa_2400_file_path  = os.path.join(Path(__file__).resolve().parents[2],
+                                     'rml',
+                                     elisa_2400_file_name+'.rml')
+
+
+
+
+
+
+########################################################################################
+
+# Multilayer efficiency
+efficiency_path = os.path.join(Path(__file__).resolve().parents[4],
+                                'multilayer_monochromator_efficiency',
+                                'calculate_ml',
+                                'Ni_B4C - 40 layers', 
+                                'Ni_B4C_2400lmm_2order.csv')
+
+
+efficiency_2400 = pd.read_csv(efficiency_path)
+efficiency_2400 = efficiency_2400[efficiency_2400['Energy[eV]']<=6000]
+cff_2400    = efficiency_2400['cff'].to_numpy().flatten()
+energy_2400 = efficiency_2400['Energy[eV]'].to_numpy().flatten()
+
---
+++ beamlines/Signature2-Liquid_Interface/simulations/comparison_with_BII/eval_2400.py
@@ -0,0 +1,212 @@
+import os
+import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter
+
+import pandas as pd
+
+from parameter3 import SlitSize_2400 as SlitSize
+from parameter3 import undulator as undulator_3
+
+from params import undulator as undulator_2
+from raypyng.postprocessing import PostProcessAnalyzed
+
+p = PostProcessAnalyzed()
+mov_av = p.moving_average
+##############################################################
+# LOAD IN DATA
+
+# Read CSV-File of the Beamline Simulation
+BL_file_path = os.path.join('../characterization/RAYPy_Simulation_sotexs_2400', 'DetectorAtFocus_RawRaysOutgoing.csv')
+BL_df = pd.read_csv(BL_file_path)
+
+# Read CSV-File of the Beamline Simulation
+BL_file_path = os.path.join('RAYPy_Simulation_2400', 'DetectorAtFocus_RawRaysOutgoing.csv')
+BL_df_2 = pd.read_csv(BL_file_path)
+
+harms=[1,3,5]
+##############################################################
+# PLOTTING AND ANALYSIS
+plt.rcParams.update({'font.size': 15})  # Change 14 to any size you prefer
+# Create the Main figure
+fig, (axs) = plt.subplots(3, 2, figsize=(20, 15))
+fig.suptitle('Comparison SoTeXS @ BESSY II and BESSY III, 2400 l/mm')
+x_range = [500, 8000]
+
+
+# TRANSMITTED BANDWIDTH
+ax3 = axs[0, 0]
+
+window = 20
+colors = ['red', 'blue', 'green']
+color_B2 = ['salmon', 'skyblue', 'lightgreen']
+for ind,harm in enumerate(harms):
+    Emin_harm = undulator_3[f'Energy{harm}[eV]'].min()
+    Emax_harm = undulator_3[f'Energy{harm}[eV]'].max()
+    filtered_df = BL_df[(BL_df['PhotonEnergy'] >= Emin_harm) & (BL_df['PhotonEnergy'] <= Emax_harm)]
+    ax3.plot(mov_av(filtered_df['PhotonEnergy'], window), 
+             mov_av(filtered_df['Bandwidth']*1000, window),
+             color=colors[ind], linestyle='solid',
+             label=f'B3-Harm. {harm}')
+    Emin_harm = undulator_2[f'Energy{harm}[eV]'].min()
+    Emax_harm = undulator_2[f'Energy{harm}[eV]'].max()
+    filtered_df = BL_df_2[(BL_df_2['PhotonEnergy'] >= Emin_harm) & (BL_df_2['PhotonEnergy'] <= Emax_harm)]
+    ax3.plot(mov_av(filtered_df['PhotonEnergy'], window), 
+                mov_av(filtered_df['Bandwidth']*1000, window),
+                color=color_B2[ind], linestyle='dashed',
+                label=f'B2-Harm. {harm}')
+
+
+ax3.set_title(f'Transmitted Bandwidth @{int(SlitSize[0]*1000)} µm ExitSlit')
+ax3.set_xlabel('Energy [eV]')
+ax3.set_ylabel('Transmitted bandwidth [meV]')
+ax3.legend(loc='best')
+ax3.set_xlim(x_range)
+ax3.minorticks_on()
+ax3.grid(which='major', axis='x', linestyle='--', linewidth=0.5, color='lightgrey')
+
+
+# BEAMLINE FLUX CURVE
+ax4 = axs[0, 1]
+window = 1
+for ind,harm in enumerate(harms):
+    Emin_harm = undulator_3[f'Energy{harm}[eV]'].min()
+    Emax_harm = undulator_3[f'Energy{harm}[eV]'].max()
+    filtered_df = BL_df[(BL_df['PhotonEnergy'] >= Emin_harm) & (BL_df['PhotonEnergy'] <= Emax_harm)]
+    ax4.plot(mov_av(filtered_df['PhotonEnergy'], window), 
+             mov_av(filtered_df[f'PhotonFlux{harm}'], window),
+             color=colors[ind], linestyle='solid',
+             label=f'B3-Harm. {harm}')
+    Emin_harm = undulator_2[f'Energy{harm}[eV]'].min()
+    Emax_harm = undulator_2[f'Energy{harm}[eV]'].max()
+    filtered_df = BL_df_2[(BL_df_2['PhotonEnergy'] >= Emin_harm) & (BL_df_2['PhotonEnergy'] <= Emax_harm)]
+    ax4.plot(mov_av(filtered_df['PhotonEnergy'], window), 
+                mov_av(filtered_df[f'PhotonFlux{harm}'], window),
+                color=color_B2[ind], linestyle='dashed',
+                label=f'B2-Harm. {harm}')
+    
+
+ax4.set_title('Flux with CPMU21')
+ax4.set_xlabel('Energy [eV]')
+ax4.set_ylabel('Photon flux [ph/s/300 mA/TBW]')
+ax4.legend(loc='best')
+ax4.set_xlim(x_range)
+ax4.minorticks_on()
+ax4.grid(which='major', axis='x', linestyle='--', linewidth=0.5, color='lightgrey')
+ax4.set_yscale('log')
+
+# RESOLVING POWER
+ax5 = axs[1, 0]
+window = 20
+for ind,harm in enumerate(harms):
+    Emin_harm = undulator_3[f'Energy{harm}[eV]'].min()
+    Emax_harm = undulator_3[f'Energy{harm}[eV]'].max()
+    filtered_df = BL_df[(BL_df['PhotonEnergy'] >= Emin_harm) & (BL_df['PhotonEnergy'] <= Emax_harm)]
+    ax5.plot(mov_av(filtered_df['PhotonEnergy'], window),
+             mov_av(filtered_df[f'PhotonEnergy']/filtered_df[f'Bandwidth'], window),
+             color=colors[ind], linestyle='solid',
+             label=f'B3-Harm. {harm}')
+    Emin_harm = undulator_2[f'Energy{harm}[eV]'].min()
+    Emax_harm = undulator_2[f'Energy{harm}[eV]'].max()
+    filtered_df = BL_df_2[(BL_df_2['PhotonEnergy'] >= Emin_harm) & (BL_df_2['PhotonEnergy'] <= Emax_harm)]
+    ax5.plot(mov_av(filtered_df['PhotonEnergy'], window),
+             mov_av(filtered_df[f'PhotonEnergy']/filtered_df[f'Bandwidth'], window),
+                color=color_B2[ind], linestyle='dashed',
+                label=f'B2-Harm. {harm}')
+
+ax5.set_title(f'Resolving Power @ {int(SlitSize[0]*1000)} µm ExitSlit')
+ax5.set_xlabel('Energy [eV]')
+ax5.set_ylabel(r'$\frac{E}{\Delta E}$ [a.u.]')
+ax5.legend(loc='best')
+ax5.set_xlim(x_range)
+ax5.minorticks_on()
+ax5.grid(which='major', axis='x', linestyle='--', linewidth=0.5, color='lightgrey')
+
+
+# Flux Density
+ax6 = axs[1, 1]
+
+for ind,harm in enumerate(harms):
+    Emin_harm = undulator_3[f'Energy{harm}[eV]'].min()
+    Emax_harm = undulator_3[f'Energy{harm}[eV]'].max()
+    filtered_df = BL_df[(BL_df['PhotonEnergy'] >= Emin_harm) & (BL_df['PhotonEnergy'] <= Emax_harm)]
+    foc_area = (filtered_df['VerticalFocusFWHM']*filtered_df['HorizontalFocusFWHM'])*1000
+    ax6.plot(filtered_df['PhotonEnergy'],
+             filtered_df[f'PhotonFlux{harm}']/foc_area,
+             color=colors[ind], linestyle='solid',
+             label=f'B3-Harm. {harm}')
+    Emin_harm = undulator_2[f'Energy{harm}[eV]'].min()
+    Emax_harm = undulator_2[f'Energy{harm}[eV]'].max()
+    filtered_df = BL_df_2[(BL_df_2['PhotonEnergy'] >= Emin_harm) & (BL_df_2['PhotonEnergy'] <= Emax_harm)]
+    foc_area = (filtered_df['VerticalFocusFWHM']*filtered_df['HorizontalFocusFWHM'])*1000
+    ax6.plot(filtered_df['PhotonEnergy'],
+             filtered_df[f'PhotonFlux{harm}']/foc_area,
+                color=color_B2[ind], linestyle='dashed',
+                label=f'B2-Harm. {harm}')
+    
+ax6.set_title('Flux Density')
+ax6.set_xlabel('Energy [eV]')
+ax6.set_ylabel('Photons flux per µm²')
+ax6.legend(loc='best')
+ax6.set_xlim(x_range)
+ax6.minorticks_on()
+ax6.set_yscale('log')
+ax6.grid(which='major', axis='x', linestyle='--', linewidth=0.5, color='lightgrey')
+
+# Horizontal Focus Size
+ax7 = axs[2, 0]
+ax7.plot(mov_av(BL_df['PhotonEnergy'], window),
+         mov_av(BL_df['HorizontalFocusFWHM']*1000, window),
+         label='B3', color='purple', linestyle='solid')
+
+ax7.plot(mov_av(BL_df_2['PhotonEnergy'], window),
+         mov_av(BL_df_2['HorizontalFocusFWHM']*1000, window),
+         label='B2', color='violet', linestyle='dashed')
+
+ax7.set_title('Horizontal Focus Size')
+ax7.set_xlabel('Energy [eV]')
+ax7.set_ylabel('[µm]')
+ax7.set_ylim(0.01, 10.5)
+ax7.set_yscale('log')
+ax7.set_xlim(x_range)
+ax7.minorticks_on()
+ax7.grid(which='major', axis='x', linestyle='--', linewidth=0.5, color='lightgrey')
+ax7.yaxis.set_major_formatter(ScalarFormatter())
+ax7.ticklabel_format(style='plain', axis='y')
+ax7.legend(loc='best')
+# Vertical Focus Size
+ax8 = axs[2, 1]
+
+ax8.plot(mov_av(BL_df['PhotonEnergy'], window),
+         mov_av(BL_df['VerticalFocusFWHM']*1000, window),
+         label='B3', color='brown', linestyle='solid')
+
+ax8.plot(mov_av(BL_df_2['PhotonEnergy'], window),
+         mov_av(BL_df_2['VerticalFocusFWHM']*1000, window),
+         label='B2', color='peru', linestyle='dashed')
+
+ax8.set_title('Vertical Focus Size')
+ax8.set_xlabel('Energy [eV]')
+ax8.set_ylabel('[µm]')
+ax8.set_xlim(x_range)
+ax8.set_ylim(0.01, 10.5)
+ax8.set_yscale('log')
+ax8.minorticks_on()
+ax8.grid(which='major', axis='x', linestyle='--', linewidth=0.5, color='lightgrey')
+ax8.yaxis.set_major_formatter(ScalarFormatter())
+ax8.ticklabel_format(style='plain', axis='y')
+ax8.legend(loc='best')
+
+
+##############################################################
+# SAVING
+# Ensure the "plot" folder exists
+plot_folder = 'plot'
+if not os.path.exists(plot_folder):
+    os.makedirs(plot_folder)
+
+# Save the the figure
+plt.tight_layout()
+plt.savefig('plot/SoTeXS_2400_comparison.png')
+plt.show()
+plt.close()
+
