8bffa9a14ce4908bb48857cbc11f218a37a5501a
Simone Vadilonga <simone.vadilonga@helmholtz-berlin.de>
2025-09-25 10:51:17 +0200

    sim

--- beamlines/Dipole-PGM/simulations/characterization/run_scripts_select.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python simulation_400.py
-python simulation_1200.py
-python simulation_2400.py
-
-
-
-    
--- beamlines/Dipole-PGM/simulations/run_scripts.sh
+++
@@ -1,9 +0,0 @@
-# Run all *simulation*.py scripts in all subdirectories
-
-find . -type f -name '*simulation*.py' | sort -f | while read -r py_file
-do
-    dir=$(dirname "$py_file")
-    base=$(basename "$py_file")
-    echo "Running $base in $dir"
-    (cd "$dir" && python "$base")
-done
