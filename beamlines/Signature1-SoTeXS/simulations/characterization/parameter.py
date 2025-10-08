f12dbb9d9fc5ea83c86454151258aa69381f815f
Simone Vadilonga <simone.vadilonga@helmholtz-berlin.de>
2025-10-08 08:55:19 +0200

    prepare sotexs sim using IVU28

--- beamlines/Signature1-SoTeXS/simulations/characterization/parameter.py
+++ beamlines/Signature1-SoTeXS/simulations/characterization/parameter.py
@@ -7,21 +7,15 @@ from pathlib import Path
 this_file_dir   = os.path.dirname(os.path.realpath(__file__))
 ncpu = 30
 nrays=1e5
-rounds = 20
+rounds = 1
 
 # UNDULATOR
-undulator_file_path_cpmu21 = os.path.abspath(
-    os.path.join(Path(__file__).resolve().parents[4], 
-                 'undulators',
-                 'UndulatorFiles_BESSY_III',
-                 'CPMU21_300mA_100pmrad_x_100pmrad.csv')
-)
+
 undulator_file_path = os.path.abspath(
     os.path.join(Path(__file__).resolve().parents[4], 
                  'undulators',
                  'UndulatorFiles_BESSY_III',
-                 'undulator_flux_curves_SPECTRA',
-                 'UE42p5_b3_2PercCoupl_2025_smalerz_ver_300mA.csv')
+                 'SoTeXS-IVU28-BIII-1.csv')
 )
 
 undulator = pd.read_csv(undulator_file_path)
