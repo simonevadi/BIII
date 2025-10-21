45688e9b2d53b8efa4a4e3ccb40ef578a9228acc
Simone Vadilonga <simone.vadilonga@helmholtz-berlin.de>
2025-10-21 09:48:42 +0200

    eval

--- beamlines/Signature2-Liquid_Interface/simulations/comparison_with_sotexs_BII/eval_2400_talk.py
+++ beamlines/Signature2-Liquid_Interface/simulations/comparison_with_sotexs_BII/eval_2400_talk.py
@@ -24,95 +24,106 @@ mov_av = p.moving_average
 # LOAD IN DATA
 
 # Read CSV-File of the Beamline Simulation
-BL_file_path = os.path.join('../../../Signature1-SoTeXS/simulations/characterization/RAYPy_Simulation_sotexs_2400', 'DetectorAtFocus_RawRaysOutgoing.csv')
+BL_file_path = os.path.join('../characterization/RAYPy_Simulation_elisa_2400', 'DetectorAtFocus_RawRaysOutgoing.csv')
 BL_df = pd.read_csv(BL_file_path)
 
+# Read CSV-File of the Beamline Simulation
+BL_file_path = os.path.join('../characterization/RAYPy_Simulation_elisa_1200', 'DetectorAtFocus_RawRaysOutgoing.csv')
+BL_df_1200 = pd.read_csv(BL_file_path)
+BL_df_1200_5 = BL_df_1200[BL_df_1200['PG.cFactor'] == 5]
+BL_df_1200 = BL_df_1200[BL_df_1200['PG.cFactor'] == 2.25]
 
 # Read CSV-File of the Beamline Simulation
 BL_file_path = os.path.join('RAYPy_Simulation_2400', 'DetectorAtFocus_RawRaysOutgoing.csv')
 BL_df_2 = pd.read_csv(BL_file_path)
 
+# Read CSV-File of the Beamline Simulation
+BL_file_path = os.path.join('RAYPy_Simulation_1200_Pt', 'DetectorAtFocus_RawRaysOutgoing.csv')
+BL_df_2_1200 = pd.read_csv(BL_file_path)
+BL_df_2_1200 = BL_df_2_1200[BL_df_2_1200['PhotonEnergy'] <= 2000]
+
 harms=[1,3,5]
-##############################################################
-# PLOTTING AND ANALYSIS
-plt.rcParams.update({'font.size': 15})  # Change 14 to any size you prefer
-# Create the Main figure
-fig, (axs) = plt.subplots(1, 2, figsize=(20, 7))
-fig.suptitle('Comparison Liquid Interface @ BESSY III and SoTeXS @ BESSY II, 2400 l/mm')
 x_range = [200, 6200]
 
-# MIRROR REFLECTIVITY
-ax1 = axs[0]
-# Coatings:
-de = 0.01
-table = 'Henke'
-theta = 0.4
-E = np.arange(x_range[0], x_range[-1], de)
-Ir  = rm.Material('Ir',  rho=22.56, kind='mirror',table=table)
-Cr  = rm.Material('Cr',  rho=7.15,  kind='mirror',table=table)
-B4C = rm.Material('C',   rho=2.52,  kind='mirror',table=table)
-IrCrB4C = rm.Multilayer(tLayer=B4C, tThickness=40, 
-                        bLayer=Cr, bThickness=60, 
-                        nPairs=1, substrate=Ir)
+# ##############################################################
+# # PLOTTING AND ANALYSIS
+# plt.rcParams.update({'font.size': 15})  # Change 14 to any size you prefer
+# # Create the Main figure
+# fig, (axs) = plt.subplots(1, 2, figsize=(20, 7))
+# fig.suptitle('Comparison Liquid Interface @ BESSY III and SoTeXS @ BESSY II, 2400 l/mm')
 
-Ir, _ = get_reflectivity(Ir, E=E, theta=theta)
-Cr, _ = get_reflectivity(Cr, E=E, theta=theta)
-B4C, _ = get_reflectivity(B4C, E=E, theta=theta)
-IrCrB4C, _ = get_reflectivity(IrCrB4C, E=E, theta=theta)
+# # MIRROR REFLECTIVITY
+# ax1 = axs[0]
+# # Coatings:
+# de = 0.01
+# table = 'Henke'
+# theta = 0.4
+# E = np.arange(x_range[0], x_range[-1], de)
+# Ir  = rm.Material('Ir',  rho=22.56, kind='mirror',table=table)
+# Cr  = rm.Material('Cr',  rho=7.15,  kind='mirror',table=table)
+# B4C = rm.Material('C',   rho=2.52,  kind='mirror',table=table)
+# IrCrB4C = rm.Multilayer(tLayer=B4C, tThickness=40, 
+#                         bLayer=Cr, bThickness=60, 
+#                         nPairs=1, substrate=Ir)
 
-ax1.plot(E, Ir, 'gold', label='Ir', alpha=0.5)
-ax1.plot(E, Cr, 'blue', label='Cr', alpha=0.5)
-ax1.plot(E, B4C, 'red', label='B4C', alpha=0.5)
-ax1.plot(E, IrCrB4C, 'black', label='IrCrB4C', linewidth=2)
+# Ir, _ = get_reflectivity(Ir, E=E, theta=theta)
+# Cr, _ = get_reflectivity(Cr, E=E, theta=theta)
+# B4C, _ = get_reflectivity(B4C, E=E, theta=theta)
+# IrCrB4C, _ = get_reflectivity(IrCrB4C, E=E, theta=theta)
 
-ax1.set_title('Mirror Coating Reflectivity @ 'f'{theta}° incident angle')
-ax1.set_xlabel('Energy [eV]')
-ax1.set_ylabel('Reflectivity [a.u.]')
-ax1.legend()
-ax1.set_xlim(x_range)
-ax1.minorticks_on()
-ax1.grid(which='major', axis='x', linestyle='--', linewidth=0.5, color='lightgrey')
+# ax1.plot(E, Ir, 'gold', label='Ir', alpha=0.5)
+# ax1.plot(E, Cr, 'blue', label='Cr', alpha=0.5)
+# ax1.plot(E, B4C, 'red', label='B4C', alpha=0.5)
+# ax1.plot(E, IrCrB4C, 'black', label='IrCrB4C', linewidth=2)
 
-
-
-ax3 = axs[0]
-
-window = 20
-colors = ['red', 'blue', 'green']
-color_B2 = ['salmon', 'skyblue', 'lightgreen']
+# ax1.set_title('Mirror Coating Reflectivity @ 'f'{theta}° incident angle')
+# ax1.set_xlabel('Energy [eV]')
+# ax1.set_ylabel('Reflectivity [a.u.]')
+# ax1.legend()
+# ax1.set_xlim(x_range)
+# ax1.minorticks_on()
+# ax1.grid(which='major', axis='x', linestyle='--', linewidth=0.5, color='lightgrey')
 
 
-ax3 = axs[1]
-for ind,harm in enumerate(harms):
-    ax3.plot(undulator_3[f'Energy{harm}[eV]'], 
-             undulator_3[f'Photons{harm}'],
-             color=colors[ind], linestyle='solid',
-             label=f'B3-Harm. {harm}')
-for ind,harm in enumerate(harms):
-    ax3.plot(undulator_2[f'Energy{harm}[eV]'], 
-             undulator_2[f'Photons{harm}'],
-             color=colors[ind], linestyle='dashed',
-             label=f'B2-Harm. {harm}')
+
+# ax3 = axs[0]
+
+# window = 20
+# colors = ['red', 'blue', 'green']
+# color_B2 = ['salmon', 'skyblue', 'lightgreen']
+
+
+# ax3 = axs[1]
+# for ind,harm in enumerate(harms):
+#     ax3.plot(undulator_3[f'Energy{harm}[eV]'], 
+#              undulator_3[f'Photons{harm}'],
+#              color=colors[ind], linestyle='solid',
+#              label=f'B3-Harm. {harm}')
+# for ind,harm in enumerate(harms):
+#     ax3.plot(undulator_2[f'Energy{harm}[eV]'], 
+#              undulator_2[f'Photons{harm}'],
+#              color=colors[ind], linestyle='dashed',
+#              label=f'B2-Harm. {harm}')
     
-ax3.legend()
-ax3.set_yscale('log')
-ax3.set_xlabel('Energy [eV]')
-ax3.set_ylabel('Ph/s/0.3A/0.1%BW')
-ax3.set_xlim(x_range)
-ax3.set_ylim(9e7, 5e15)
-ax3.set_title('IVUE42 @ BESSY III vs CPMU21 @ BESSY II')
+# ax3.legend()
+# ax3.set_yscale('log')
+# ax3.set_xlabel('Energy [eV]')
+# ax3.set_ylabel('Ph/s/0.3A/0.1%BW')
+# ax3.set_xlim(x_range)
+# ax3.set_ylim(9e7, 5e15)
+# ax3.set_title('IVUE42 @ BESSY III vs CPMU21 @ BESSY II')
 
-# SAVING
-# Ensure the "plot" folder exists
-plot_folder = 'plot'
-if not os.path.exists(plot_folder):
-    os.makedirs(plot_folder)
+# # SAVING
+# # Ensure the "plot" folder exists
+# plot_folder = 'plot'
+# if not os.path.exists(plot_folder):
+#     os.makedirs(plot_folder)
 
-# Save the the figure
-plt.tight_layout()
-plt.savefig('plot/talk/LiquidInterface_2400_comparison_1.png')
-# plt.show()
-plt.close()
+# # Save the the figure
+# plt.tight_layout()
+# plt.savefig('plot/talk/LiquidInterface_2400_comparison_1.png')
+# # plt.show()
+# plt.close()
 
 
 
@@ -126,22 +137,33 @@ fig.suptitle('Comparison SoTeXS @ BESSY II and BESSY III, 2400 l/mm')
 # TRANSMITTED BANDWIDTH
 ax3 = axs[0]
 
-window = 20
+window = 30
 colors = ['red', 'blue', 'green']
 color_B2 = ['salmon', 'skyblue', 'lightgreen']
 
 ax3.plot(mov_av(BL_df['PhotonEnergy'], window), 
              mov_av(BL_df['Bandwidth']*1000, window),
              linestyle='solid',
-             label=f'B3 - nano focus',
+             label=f'B3 - 2400 l/mm',
              color='blue')
+
+ax3.plot(mov_av(BL_df_1200['PhotonEnergy'], window), 
+             mov_av(BL_df_1200['Bandwidth']*1000, window),
+             linestyle='solid',
+             label=f'B3 - 1200 l/mm - cff=2.25',
+             color='cyan')
 
 ax3.plot(mov_av(BL_df_2['PhotonEnergy'], window), 
              mov_av(BL_df_2['Bandwidth']*1000, window),
              linestyle='solid',
-             label=f'B2', 
+             label=f'B2 - 2400l/mm', 
              color='green')
 
+ax3.plot(mov_av(BL_df_2_1200['PhotonEnergy'], window), 
+             mov_av(BL_df_2_1200['Bandwidth']*1000, window),
+             linestyle='solid',
+             label=f'B2 - 1200l/mm - cff=2.25', 
+             color='lime')
 
 
 ax3.set_title(f'Transmitted Bandwidth @{int(SlitSize[0]*1000)} µm ExitSlit')
@@ -163,7 +185,17 @@ for ind,harm in enumerate(harms):
     ax4.plot(mov_av(filtered_df['PhotonEnergy'], window), 
              mov_av(filtered_df[f'PhotonFlux{harm}'], window),
              color='blue', linestyle='solid',
-             label=f'B3-nano')
+             label=f'B3 2400 l/mm')
+
+for ind,harm in enumerate([1]):
+    Emin_harm = undulator_3[f'Energy{harm}[eV]'].min()
+    Emax_harm = undulator_3[f'Energy{harm}[eV]'].max()
+    filtered_df = BL_df_1200[(BL_df_1200['PhotonEnergy'] >= Emin_harm) & (BL_df_1200['PhotonEnergy'] <= Emax_harm)]
+    ax4.plot(mov_av(filtered_df['PhotonEnergy'], window), 
+             mov_av(filtered_df[f'PhotonFlux{harm}'], window),
+             color='cyan', linestyle='solid',
+             label=f'B3 1200 l/mm')
+    
     
 for ind,harm in enumerate(harms):    
     Emin_harm = undulator_2[f'Energy{harm}[eV]'].min()
@@ -173,22 +205,31 @@ for ind,harm in enumerate(harms):
                 mov_av(filtered_df[f'PhotonFlux{harm}'], window),
                 color='green', linestyle='dashed',
                 label=f'B2-Harm. {harm}')
-    
+
+for ind,harm in enumerate([1]):
+    Emin_harm = undulator_2[f'Energy{harm}[eV]'].min()
+    Emax_harm = undulator_2[f'Energy{harm}[eV]'].max()
+    filtered_df = BL_df_2_1200[(BL_df_2_1200['PhotonEnergy'] >= Emin_harm) & (BL_df_2_1200['PhotonEnergy'] <= Emax_harm)]
+    ax4.plot(mov_av(filtered_df['PhotonEnergy'], window), 
+             mov_av(filtered_df[f'PhotonFlux{harm}'], window),
+             color='lime', linestyle='solid',
+             label=f'B2 1200 l/mm')    
 
 ax4.set_title('Flux at Focus')
 ax4.set_xlabel('Energy [eV]')
 ax4.set_ylabel('Photon flux [ph/s/300 mA/TBW]')
 custom_lines = [
     Line2D([0], [0], color='blue', linestyle='solid', lw=2),
-    Line2D([0], [0], color='orange', linestyle='solid', lw=2),
-    Line2D([0], [0], color='green', linestyle='solid', lw=2)
+    Line2D([0], [0], color='cyan', linestyle='solid', lw=2),
+    Line2D([0], [0], color='green', linestyle='solid', lw=2),
+    Line2D([0], [0], color='lime', linestyle='solid', lw=2)
 ]
 
-ax4.legend(custom_lines, ['B3-nano', 'B3-micro', 'B2'], loc='best')
+ax4.legend(custom_lines, ['B3 - 2400 l/mm', 'B3 - 1200 l/mm', 'B2 - 2400 l/mm', 'B2 - 1200 l/mm'], loc='best')
 ax4.set_xlim(x_range)
 ax4.minorticks_on()
 ax4.grid(which='major', axis='x', linestyle='--', linewidth=0.5, color='lightgrey')
-ax4.set_ylim((10e9,1e15))
+ax4.set_ylim((1e7,1e14))
 ax4.set_yscale('log')
 
 # Save the the figure
@@ -200,7 +241,114 @@ plt.close()
 
 
 
+###################################################
+fig, (axs) = plt.subplots(1, 2, figsize=(20, 7))
+fig.suptitle('Comparison SoTeXS @ BESSY II and BESSY III, 2400 l/mm')
 
+
+# TRANSMITTED BANDWIDTH
+ax3 = axs[0]
+
+window = 30
+colors = ['red', 'blue', 'green']
+color_B2 = ['salmon', 'skyblue', 'lightgreen']
+
+ax3.plot(mov_av(BL_df['PhotonEnergy'], window), 
+             mov_av(BL_df['PhotonEnergy']/BL_df['Bandwidth'], window),
+             linestyle='solid',
+             label=f'B3 - 2400 l/mm',
+             color='blue')
+
+ax3.plot(mov_av(BL_df_1200['PhotonEnergy'], window), 
+             mov_av(BL_df_1200['PhotonEnergy']/BL_df_1200['Bandwidth'], window),
+             linestyle='solid',
+             label=f'B3 - 1200 l/mm - cff=2.25',
+             color='cyan')
+
+ax3.plot(mov_av(BL_df_2['PhotonEnergy'], window), 
+             mov_av(BL_df_2['PhotonEnergy']/BL_df_2['Bandwidth'], window),
+             linestyle='solid',
+             label=f'B2 - 2400l/mm', 
+             color='green')
+
+ax3.plot(mov_av(BL_df_2_1200['PhotonEnergy'], window), 
+             mov_av(BL_df_2_1200['PhotonEnergy']/BL_df_2_1200['Bandwidth'], window),
+             linestyle='solid',
+             label=f'B2 - 1200l/mm - cff=2.25', 
+             color='lime')
+
+
+ax3.set_title(f'Resolving Power @{int(SlitSize[0]*1000)} µm ExitSlit')
+ax3.set_xlabel('Energy [eV]')
+ax3.set_ylabel('Resolving Power [a.u.]')
+ax3.legend(loc='best')
+ax3.set_xlim(x_range)
+ax3.minorticks_on()
+ax3.grid(which='major', axis='x', linestyle='--', linewidth=0.5, color='lightgrey')
+
+
+# BEAMLINE FLUX CURVE
+ax4 = axs[1]
+window = 1
+for ind,harm in enumerate(harms):
+    Emin_harm = undulator_3[f'Energy{harm}[eV]'].min()
+    Emax_harm = undulator_3[f'Energy{harm}[eV]'].max()
+    filtered_df = BL_df[(BL_df['PhotonEnergy'] >= Emin_harm) & (BL_df['PhotonEnergy'] <= Emax_harm)]
+    ax4.plot(mov_av(filtered_df['PhotonEnergy'], window), 
+             mov_av(filtered_df[f'PhotonFlux{harm}'], window),
+             color='blue', linestyle='solid',
+             label=f'B3 2400 l/mm')
+
+for ind,harm in enumerate([1]):
+    Emin_harm = undulator_3[f'Energy{harm}[eV]'].min()
+    Emax_harm = undulator_3[f'Energy{harm}[eV]'].max()
+    filtered_df = BL_df_1200[(BL_df_1200['PhotonEnergy'] >= Emin_harm) & (BL_df_1200['PhotonEnergy'] <= Emax_harm)]
+    ax4.plot(mov_av(filtered_df['PhotonEnergy'], window), 
+             mov_av(filtered_df[f'PhotonFlux{harm}'], window),
+             color='cyan', linestyle='solid',
+             label=f'B3 1200 l/mm')
+    
+    
+for ind,harm in enumerate(harms):    
+    Emin_harm = undulator_2[f'Energy{harm}[eV]'].min()
+    Emax_harm = undulator_2[f'Energy{harm}[eV]'].max()
+    filtered_df = BL_df_2[(BL_df_2['PhotonEnergy'] >= Emin_harm) & (BL_df_2['PhotonEnergy'] <= Emax_harm)]
+    ax4.plot(mov_av(filtered_df['PhotonEnergy'], window), 
+                mov_av(filtered_df[f'PhotonFlux{harm}'], window),
+                color='green', linestyle='dashed',
+                label=f'B2-Harm. {harm}')
+
+for ind,harm in enumerate([1]):
+    Emin_harm = undulator_2[f'Energy{harm}[eV]'].min()
+    Emax_harm = undulator_2[f'Energy{harm}[eV]'].max()
+    filtered_df = BL_df_2_1200[(BL_df_2_1200['PhotonEnergy'] >= Emin_harm) & (BL_df_2_1200['PhotonEnergy'] <= Emax_harm)]
+    ax4.plot(mov_av(filtered_df['PhotonEnergy'], window), 
+             mov_av(filtered_df[f'PhotonFlux{harm}'], window),
+             color='lime', linestyle='solid',
+             label=f'B2 1200 l/mm')    
+
+ax4.set_title('Flux at Focus')
+ax4.set_xlabel('Energy [eV]')
+ax4.set_ylabel('Photon flux [ph/s/300 mA/TBW]')
+custom_lines = [
+    Line2D([0], [0], color='blue', linestyle='solid', lw=2),
+    Line2D([0], [0], color='cyan', linestyle='solid', lw=2),
+    Line2D([0], [0], color='green', linestyle='solid', lw=2),
+    Line2D([0], [0], color='lime', linestyle='solid', lw=2)
+]
+
+ax4.legend(custom_lines, ['B3 - 2400 l/mm', 'B3 - 1200 l/mm', 'B2 - 2400 l/mm', 'B2 - 1200 l/mm'], loc='best')
+ax4.set_xlim(x_range)
+ax4.minorticks_on()
+ax4.grid(which='major', axis='x', linestyle='--', linewidth=0.5, color='lightgrey')
+ax4.set_ylim((1e7,1e14))
+ax4.set_yscale('log')
+
+# Save the the figure
+plt.tight_layout()
+plt.savefig('plot/talk/LiquidInterface_2400_comparison_3.png')
+# plt.show()
+plt.close()
 
 
 
