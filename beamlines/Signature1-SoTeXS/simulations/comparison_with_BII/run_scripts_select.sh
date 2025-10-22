38085e451101193a5b681880a715efb30cbf084d
Simone Vadilonga <simone.vadilonga@helmholtz-berlin.de>
2025-10-22 09:59:24 +0200

    sotexs BII simulation_1200_Pt

--- beamlines/Signature1-SoTeXS/simulations/comparison_with_BII/run_scripts_select.sh
+++ beamlines/Signature1-SoTeXS/simulations/comparison_with_BII/run_scripts_select.sh
@@ -6,10 +6,9 @@
 python simulation_1200_Pt.py
 git add .
 git commit -m "sotexs BII simulation_1200_Pt"
-git push origin main
+git push 
 
 python simulation_2400.py
 git add .
 git commit -m "sotexs BII simulation_2400"
-git push origin main
-
+git push 
