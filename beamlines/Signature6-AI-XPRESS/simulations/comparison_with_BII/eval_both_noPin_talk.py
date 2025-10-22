70c49b00413cffb894795c3e2fc9ab7cb6a85765
Simone Vadilonga <simone.vadilonga@helmholtz-berlin.de>
2025-10-22 16:23:06 +0200

    eval

--- beamlines/Signature6-AI-XPRESS/simulations/comparison_with_BII/eval_both_noPin_talk.py
+++ beamlines/Signature6-AI-XPRESS/simulations/comparison_with_BII/eval_both_noPin_talk.py
@@ -36,7 +36,7 @@ myspot_Si111[f'PhotonFlux'] = myspot_Si111[f'PhotonFlux']*current_factor # for 3
 plt.rcParams.update({'font.size': 13})  # Change 14 to any size you prefer
 # Create the Main figure
 fig, (axs) = plt.subplots(1, 2, figsize=(20, 7))
-fig.suptitle('Signature6: AI-XPRESS, Comparison at BII: MySpot', size=16)
+fig.suptitle('Comparison: AI-XPRESS @ BESSY III and MySPot @ BESSY II', size=16)
 x_range = [2500, 40000]
 colors = ['blue', 'red', 'green', 'orange', 'purple', 'violet']
 
@@ -59,7 +59,7 @@ IrRh, _ = get_reflectivity(IrRh, E=E, theta=theta)
 
 ax1.plot(E, Ir, 'grey', label='Ir', alpha=0.5, linewidth=1.5)
 ax1.plot(E, Rh, 'blue', label='Rh', alpha=0.5, linewidth=1.5)
-ax1.plot(E, IrRh, 'black', label='IrRh', linewidth=2)
+ax1.plot(E, IrRh, 'black', label='IrRh', linewidth=5)
 
 ax1.set_title('Mirror Coating Reflectivity @ 'f'{theta}° incident angle')
 ax1.set_xlabel('Energy [eV]')
@@ -77,11 +77,16 @@ ax2 = axs[1]
 
 ax2.plot(BL_df_si111[f'PhotonEnergy'],
             BL_df_si111[f'SourcePhotonFlux'],
-            label='BIII SB 4T')
+            label='BIII SB 4T', 
+            color='blue',
+            linewidth=5)
 
 ax2.plot(myspot_Si111[f'PhotonEnergy'],
             myspot_Si111[f'SourcePhotonFlux'],
-            label='BII WS 7T')
+            label='BII WS 7T',
+            color='royalblue', 
+            linestyle='dashed',
+)
 
 
     
@@ -94,7 +99,6 @@ ax2.set_yscale('log')
 ax2.minorticks_on()
 ax2.grid(which='major', axis='x', linestyle='--', linewidth=0.5, color='lightgrey')
 
-##############################################################
 # SAVING
 # Ensure the "plot" folder exists
 plot_folder = 'plot'
@@ -108,7 +112,7 @@ plt.savefig('plot/talk/AI_XPRESS_MySpot_comparison_noPinhole_1.png')
 plt.close()
 
 fig, (axs) = plt.subplots(1, 2, figsize=(20, 7))
-fig.suptitle('Signature6: AI-XPRESS, Comparison at BII: MySpot', size=16)
+fig.suptitle('Comparison: AI-XPRESS @ BESSY III and MySPot @ BESSY II', size=16)
 # TRANSMITTED BANDWIDTH
 ax3 = axs[0]
 
@@ -117,11 +121,14 @@ window = 2
 # Si111 BIII
 ax3.plot(mov_av(BL_df_si111['PhotonEnergy'], window), 
             mov_av(BL_df_si111['Bandwidth'], window),
-            label=f'BIII Si111')    
+            label=f'BIII Si111', 
+            color='blue', linewidth=5)    
 # Si111 BII
 ax3.plot(mov_av(myspot_Si111['PhotonEnergy'], window), 
             mov_av(myspot_Si111['Bandwidth'], window),
-            label=f'BII Si111')    
+            label=f'BII Si111', 
+            color='royalblue', 
+            linestyle='dashed')    
  
 
 ax3.set_title(f'Transmitted Bandwidth')
@@ -140,11 +147,14 @@ ax4 = axs[1]
 # BIII Si111
 ax4.plot(mov_av(BL_df_si111['PhotonEnergy'], window), 
             mov_av(BL_df_si111['PhotonFlux'], window),
-            label=f'BIII Si111')    
+            label=f'BIII Si111', 
+            color='blue', linewidth=5)    
 # BII Si111
 ax4.plot(mov_av(myspot_Si111['PhotonEnergy'], window), 
             mov_av(myspot_Si111['PhotonFlux'], window),
-            label=f'BII Si111')   
+            label=f'BII Si111', 
+            color='royalblue', linewidth=1, 
+            linestyle = 'dashed')   
 
 ax4.set_title('Flux at Focus')
 ax4.set_xlabel('Energy [eV]')
@@ -161,119 +171,69 @@ plt.savefig('plot/talk/AI_XPRESS_MySpot_comparison_noPinhole_2.png')
 plt.close()
 
 fig, (axs) = plt.subplots(1, 2, figsize=(20, 7))
-fig.suptitle('Signature6: AI-XPRESS, Comparison at BII: MySpot', size=16)
-
-# # RESOLVING POWER
-# ax5 = axs[2, 0]
-# window = 1
-
-# # BIII Si111
-# ax5.plot(mov_av(BL_df_si111['PhotonEnergy'], window),
-#          mov_av(BL_df_si111[f'PhotonEnergy']/BL_df_si111[f'Bandwidth'], window),
-#          linestyle='solid', label='BIII Si111')
-
-# # BII Si111
-# ax5.plot(mov_av(myspot_Si111['PhotonEnergy'], window),
-#          mov_av(myspot_Si111[f'PhotonEnergy']/myspot_Si111[f'Bandwidth'], window),
-#          linestyle='solid', label='BII Si111')
+fig.suptitle('Comparison: AI-XPRESS @ BESSY III and MySPot @ BESSY II', size=16)
 
 
 
-# ax5.set_title(f'Resolving Power')
-# ax5.set_xlabel('Energy [eV]')
-# ax5.set_ylabel(r'$\frac{E}{\Delta E}$ [a.u.]')
-# ax5.set_xlim(x_range)
-# ax5.legend(loc='best')
-# ax5.minorticks_on()
-# ax5.set_ylim(0, 20000)
-# ax5.grid(which='major', axis='x', linestyle='--', linewidth=0.5, color='lightgrey')
 
 
-# # Flux Density
-# ax6 = axs[2, 1]
+##############################################################
+fig, (axs) = plt.subplots(1, 2, figsize=(20, 7))
+fig.suptitle('Comparison: AI-XPRESS @ BESSY III and MySPot @ BESSY II', size=16)
+# RESOLVING POWER
+ax3 = axs[0]
+
+window = 2
+
+# Si111 BIII
+ax3.plot(mov_av(BL_df_si111['PhotonEnergy'], window), 
+            mov_av(BL_df_si111['PhotonEnergy']/BL_df_si111['Bandwidth'], window),
+            label=f'BIII Si111', 
+            color='blue', linewidth=5)    
+# Si111 BII
+ax3.plot(mov_av(myspot_Si111['PhotonEnergy'], window), 
+            mov_av(myspot_Si111['PhotonEnergy']/myspot_Si111['Bandwidth'], window),
+            label=f'BII Si111', 
+            color='royalblue', 
+            linestyle='dashed')    
+ 
+
+ax3.set_title(f'Resolving Power')
+ax3.set_xlabel('Energy [eV]')
+ax3.set_ylabel('Resolving Power [a.u.]')
+ax3.set_xlim(x_range)
+ax3.minorticks_on()
+# ax3.set_yscale('log')
+ax3.grid(which='major', axis='x', linestyle='--', linewidth=0.5, color='lightgrey')
+ax3.legend(loc='lower right', ncol=2)
 
 
-# # BIII Si111
-# foc_area = (BL_df_si111['VerticalFocusFWHM']*BL_df_si111['HorizontalFocusFWHM'])*1000  # in µm²
+# BEAMLINE FLUX CURVE
+ax4 = axs[1]
 
-# ax6.plot(BL_df_si111['PhotonEnergy'],
-#              BL_df_si111[f'PhotonFlux']/foc_area,
-#              label=f'BIII Si111')
-    
-# # BIII Si111
-# foc_area = (myspot_Si111['VerticalFocusFWHM']*myspot_Si111['HorizontalFocusFWHM'])*1000  # in µm²
+# BIII Si111
+ax4.plot(mov_av(BL_df_si111['PhotonEnergy'], window), 
+            mov_av(BL_df_si111['PhotonFlux'], window),
+            label=f'BIII Si111', 
+            color='blue', linewidth=5)    
+# BII Si111
+ax4.plot(mov_av(myspot_Si111['PhotonEnergy'], window), 
+            mov_av(myspot_Si111['PhotonFlux'], window),
+            label=f'BII Si111', 
+            color='royalblue', linewidth=1, 
+            linestyle = 'dashed')   
 
-# ax6.plot(myspot_Si111['PhotonEnergy'],
-#              myspot_Si111[f'PhotonFlux']/foc_area,
-#              label=f'BII Si111')
+ax4.set_title('Flux at Focus')
+ax4.set_xlabel('Energy [eV]')
+ax4.set_ylabel('Photon flux [ph/s/300 mA/TBW]')
+ax4.set_xlim(x_range)
+ax4.minorticks_on()
+ax4.grid(which='major', axis='x', linestyle='--', linewidth=0.5, color='lightgrey')
+ax4.set_yscale('log')
 
-
-# ax6.set_title('Flux Density')
-# ax6.set_xlabel('Energy [eV]')
-# ax6.set_ylabel('Photons flux per µm²')
-# ax6.set_xlim(x_range)
-# ax6.set_yscale('log')
-# ax6.minorticks_on()
-# ax6.grid(which='major', axis='x', linestyle='--', linewidth=0.5, color='lightgrey')
-
-# ####################################################
-# # Focus at BIII
-# ax7 = axs[3,0]
-# x_lim =(-450,450)
-# y_lim =(-450,450)
-# focus_path = os.path.join('plot', 'AI-XPRESS_DetectorAtFocus-RawRaysOutgoing.csv')
-# focus = pd.read_csv(
-#     focus_path,
-#     sep='\t',        # columns separated by tabs
-#     decimal='.',     # use comma as decimal separator
-#     skiprows=1       # skip the first line (the 'sep=' line)
-# )
-# x = focus['DetectorAtFocus_OX'] * 1e3
-# y = focus['DetectorAtFocus_OY'] * 1e3
-
-
-# ax7.scatter(x,y, s=4, color='yellow', alpha=1)
-
-# ax7.set_facecolor('#002147')
-
-
-# ax7.set_xlim(x_lim)
-# ax7.set_ylim(y_lim)
-
-# ax7.set_xlabel('µm')
-# ax7.set_ylabel('µm')
-# hor_foc = np.mean(BL_df_si111['HorizontalFocusFWHM']*1e3)
-# ver_foc = np.mean(BL_df_si111['VerticalFocusFWHM']*1e3)
-# ax7.set_title(f'AI-XPRESS Focus, FWHM(HxV) {50:.0f} x {50:.0f} µm²')
-
-# ####################################################
-# # Focus at BII
-# ax8 = axs[3,1]
-
-# focus_path = os.path.join('plot', 'MySpot_noPinhole_DetectorAtFocus-RawRaysOutgoing.csv')
-# focus = pd.read_csv(
-#     focus_path,
-#     sep='\t',        # columns separated by tabs
-#     decimal='.',     # use comma as decimal separator
-#     skiprows=1       # skip the first line (the 'sep=' line)
-# )
-# x = focus['DetectorAtFocus_OX'] * 1e3
-# y = focus['DetectorAtFocus_OY'] * 1e3
-
-# ax8.scatter(x,y, s=4, color='yellow', alpha=1)
-
-# ax8.set_facecolor('#002147')
-
-
-# ax8.set_xlim(x_lim)
-# ax8.set_ylim(y_lim)
-
-# ax8.set_xlabel('µm')
-# ax8.set_ylabel('µm')
-# hor_foc = np.mean(BL_df_si111['HorizontalFocusFWHM']*1e3)
-# ver_foc = np.mean(BL_df_si111['VerticalFocusFWHM']*1e3)
-# ax8.set_title(f'MySpot Focus, FWHM(HxV) {270:.0f} x {150:.0f} µm²')
-
-
-
+# Save the the figure
+plt.tight_layout()
+plt.savefig('plot/talk/AI_XPRESS_MySpot_comparison_noPinhole_3.png')
+# plt.show()
+plt.close()
 
